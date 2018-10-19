0bdeac26ca89c349d7bbc68b61514dd2543ea8a2
Ricardo Ledesma <rickydem24@gmail.com>
2018-10-19 10:41:05 -0600

    Adds active_players_by_position to WorldCup

--- world_cup/lib/world_cup.rb
+++ world_cup/lib/world_cup.rb
@@ -3,4 +3,11 @@ class WorldCup
     @year = year
     @teams = teams
   end
+
+  def active_players_by_position(position)
+    @teams.reduce([]) do |active_players, team|
+      active_players.concat(team.players_by_position(position)) if not team.eliminated
+      active_players
+    end
+  end
 end
