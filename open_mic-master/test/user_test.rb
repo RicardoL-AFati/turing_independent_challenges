a1e29f1c38d3228f275de96c267fa260f7468433
Ricardo Ledesma <rickydem24@gmail.com>
2018-10-20 15:23:46 -0600

    Adds testing for instance variable jokes and learn method

--- open_mic-master/test/user_test.rb
+++ open_mic-master/test/user_test.rb
@@ -1,24 +1,41 @@
-require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/user'
+require './lib/joke'
 
 class UserTest < Minitest::Test
-  def test_it_exists
-    sal = User.new("Sal")
+  def setup
+    @sal = User.new("Sal")
 
-    assert_instance_of User, sal
+    @joke_1 = Joke.new({
+      id: 1,
+      question: "Why did the strawberry cross the road?",
+      answer: "Because his mother was in a jam."
+    })
+  end
+
+  def test_it_exists
+    assert_instance_of User, @sal
   end
 
   def test_it_has_a_name
-    sal = User.new("Sal")
-
-    assert_equal "Sal", sal.name
+    assert_equal "Sal", @sal.name
   end
 
   def test_it_can_have_another_name
     ali = User.new("Ali")
 
     assert_equal "Ali", ali.name
+  end
+
+  def test_it_has_no_jokes_to_start
+    assert_empty @sal.jokes
+  end
+
+  def test_it_can_learn_a_joke
+    assert_empty @sal.jokes
+
+    @sal.learn(@joke_1)
+    assert_equal [@joke_1], @sal.jokes
   end
 end
