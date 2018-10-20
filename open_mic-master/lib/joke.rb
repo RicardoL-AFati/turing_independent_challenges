79ecd13565fa21232b49979eb089eaeaaa1cb37a
Ricardo Ledesma <rickydem24@gmail.com>
2018-10-20 15:16:28 -0600

    Adds intialize method

--- open_mic-master/lib/joke.rb
+++ open_mic-master/lib/joke.rb
@@ -1,3 +1,8 @@
 class Joke
-
+  attr_reader :id, :question, :answer
+  def initialize(attributes)
+    @id = attributes[:id]
+    @question = attributes[:question]
+    @answer = attributes[:answer]
+  end
 end
