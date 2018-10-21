d5651fb3e5d4a714298c67549238ce4082194a2d
Ricardo Ledesma <rickydem24@gmail.com>
2018-10-21 16:45:27 -0600

    Adds refactor for exhibits - patrons array

--- museum/lib/museum.rb
+++ museum/lib/museum.rb
@@ -7,18 +7,18 @@ class Museum
   end
 
   def add_exhibit(name, cost)
-    @exhibits[name.to_sym] = {cost: cost, attendees: 0}
+    @exhibits[name.to_sym] = {cost: cost, patrons: []}
   end
 
   def admit(patron)
     @revenue += 20
-    update_exhibits_based_on_interests(patron)
+    update_exhibits_with(patron)
   end
 
-  def update_exhibits_based_on_interests(patron)
+  def update_exhibits_with(patron)
     patron.interests.each do |interest|
       if @exhibits.key?(interest.to_sym)
-        @exhibits[interest.to_sym][:attendees] += 1
+        @exhibits[interest.to_sym][:patrons] << patron.name
       end
     end
   end
