7a8880ec8bf631ef12d8fdd1d0b5846fd90a590e
Ricardo Ledesma <rickydem24@gmail.com>
2018-10-21 10:52:02 -0600

    Adds intialization for museum and tests

---
+++ museum/test/museum_test.rb
@@ -0,0 +1,17 @@
+require './lib/museum'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class MuseumTest < Minitest::Test
+  def setup
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+  end
+
+  def test_it_exists
+    assert_instance_of Museum, @dmns
+  end
+
+  def test_it_has_a_name
+    assert_equal "Denver Museum of Nature and Science", @dmns.name
+  end
+end
