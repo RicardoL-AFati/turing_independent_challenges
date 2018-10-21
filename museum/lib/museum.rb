f17caac94158aeb577228b79ca12b2b8eb6ee2b2
Ricardo Ledesma <rickydem24@gmail.com>
2018-10-21 11:07:24 -0600

    Adds exhibits and revenue instance variables and add_exhibit

--- museum/lib/museum.rb
+++ museum/lib/museum.rb
@@ -2,5 +2,11 @@ class Museum
   attr_reader :name
   def initialize(name)
     @name = name
+    @exhibits = {}
+    @revenue = 0
+  end
+
+  def add_exhibit(name, cost)
+    @exhibits[name.to_sym] = {cost: cost, attendees: 0}
   end
 end
