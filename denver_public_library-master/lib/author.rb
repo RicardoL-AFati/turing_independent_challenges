b732c1c496b074d5df153f541c0855d135c08027
Ricardo Ledesma <rickydem24@gmail.com>
2018-10-21 08:19:01 -0600

    Adds intialization for Author and testing methods

--- denver_public_library-master/lib/author.rb
+++ denver_public_library-master/lib/author.rb
@@ -1,3 +1,10 @@
 class Author
+  attr_reader :first_name, :last_name, :books
+  def initialize(attributes)
+    @first_name = attributes[:first_name]
+    @last_name = attributes[:last_name]
+    @books = []
+  end
+
 
 end
--- denver_public_library-master/test/author_test.rb
+++ denver_public_library-master/test/author_test.rb
@@ -1,6 +1,24 @@
 require './test/test_helper'
+require './lib/author'
 
 class AuthorTest < Minitest::Test
+  def setup
+    @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+  end
 
+  def test_it_exists
+    assert_instance_of Author, @charlotte_bronte
+  end
 
+  def test_it_has_a_first_name
+    assert_equal "Charlotte", @charlotte_bronte.first_name
+  end
+
+  def test_it_has_a_last_name
+    assert_equal "Bronte", @charlotte_bronte.last_name
+  end
+
+  def test_it_has_no_books_when_created
+    assert_empty @charlotte_bronte.books
+  end
 end
