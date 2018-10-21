d12e136c5f4294a2d8618bb9f7dfe6942e976a0c
Ricardo Ledesma <rickydem24@gmail.com>
2018-10-21 08:28:18 -0600

    Adds adding book method and test

--- denver_public_library-master/test/author_test.rb
+++ denver_public_library-master/test/author_test.rb
@@ -21,4 +21,23 @@ class AuthorTest < Minitest::Test
   def test_it_has_no_books_when_created
     assert_empty @charlotte_bronte.books
   end
+
+  def test_it_can_receive_books
+    assert_empty @charlotte_bronte.books
+
+    @charlotte_bronte.add_book("Jane Eyre", "October 16, 1847")
+    @charlotte_bronte.add_book("Villette", "1853")
+
+    assert_equal 2, @charlotte_bronte.books.count
+
+    assert_equal "Charlotte", @charlotte_bronte.books[0].author_first_name
+    assert_equal "Bronte", @charlotte_bronte.books[0].author_last_name
+    assert_equal "Jane Eyre", @charlotte_bronte.books[0].title
+    assert_equal "October 16, 1847", @charlotte_bronte.books[0].publication_date
+
+    assert_equal "Charlotte", @charlotte_bronte.books[1].author_first_name
+    assert_equal "Bronte", @charlotte_bronte.books[1].author_last_name
+    assert_equal "Villette", @charlotte_bronte.books[1].title
+    assert_equal "1853", @charlotte_bronte.books[1].publication_date
+  end
 end
