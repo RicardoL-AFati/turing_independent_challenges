0dad71671d018399a3694a17e27c05a8d059fbf6
Ricardo Ledesma <rickydem24@gmail.com>
2018-11-04 18:14:47 -0700

    Adds printing shopping list method

--- pantry-master/lib/pantry.rb
+++ pantry-master/lib/pantry.rb
@@ -1,8 +1,11 @@
+require 'pry'
+
 class Pantry
   attr_reader :stock, :shopping_list
   def initialize
     @stock = Hash.new(0)
     @shopping_list = Hash.new(0)
+    @cook_book = Hash.new(0)
   end
 
   def stock_check(item)
@@ -17,5 +20,15 @@ class Pantry
     recipe.ingredients.each do |ingredient, amount|
       @shopping_list[ingredient] += amount
     end
+  end
+
+  def print_shopping_list
+    list = shopping_list.map.with_index do |(ingredient, amount), index|
+      list_item = "* #{ingredient}: #{amount}"
+      list_item += "\n" unless index == shopping_list.length - 1
+      list_item
+    end.join
+    puts list
+    list
   end
 end
