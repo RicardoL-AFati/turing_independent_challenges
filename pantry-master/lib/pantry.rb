1d43009b4964585825cf52818104d99a78e946d7
Ricardo Ledesma <rickydem24@gmail.com>
2018-11-04 18:43:35 -0700

    Adds add_to_cookbook and helper method keys_to_sym

--- pantry-master/lib/pantry.rb
+++ pantry-master/lib/pantry.rb
@@ -1,11 +1,11 @@
 require 'pry'
 
 class Pantry
-  attr_reader :stock, :shopping_list
+  attr_reader :stock, :shopping_list, :cookbook
   def initialize
     @stock = Hash.new(0)
     @shopping_list = Hash.new(0)
-    @cook_book = Hash.new(0)
+    @cookbook = Hash.new(0)
   end
 
   def stock_check(item)
@@ -22,6 +22,10 @@ class Pantry
     end
   end
 
+  def add_to_cookbook(recipe)
+    @cookbook[recipe.name.to_sym] = keys_to_sym(recipe.ingredients)
+  end
+
   def print_shopping_list
     list = shopping_list.map.with_index do |(ingredient, amount), index|
       list_item = "* #{ingredient}: #{amount}"
@@ -30,5 +34,12 @@ class Pantry
     end.join
     puts list
     list
+  end
+
+  def keys_to_sym(hash)
+    hash.reduce({}) do |new_h, (k,v)|
+      new_h[k.to_sym] = v
+      new_h
+    end
   end
 end
