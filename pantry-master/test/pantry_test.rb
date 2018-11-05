0ea5d97c6612993e0866b5adaffd49a7e08ec66e
Ricardo Ledesma <rickydem24@gmail.com>
2018-11-04 19:04:55 -0700

    Adds test for how_many_can_i_make

--- pantry-master/test/pantry_test.rb
+++ pantry-master/test/pantry_test.rb
@@ -118,4 +118,19 @@ class PantryTest < Minitest::Test
 
     assert_equal ["Pickles", "Peanuts"], @pantry.what_can_i_make
   end
+
+  def test_it_returns_how_many_of_recipes_you_can_make_based_on_stock
+    @pantry.add_to_cookbook(@r0)
+    @pantry.add_to_cookbook(@r2)
+    @pantry.add_to_cookbook(@r3)
+
+    @pantry.restock("Cheese", 10)
+    @pantry.restock("Flour", 20)
+    @pantry.restock("Brine", 40)
+    @pantry.restock("Cucumbers", 120)
+    @pantry.restock("Raw nuts", 20)
+    @pantry.restock("Salt", 20)
+
+    assert_equal ({"Pickles" => 4, "Peanuts" => 2}), @pantry.how_many_can_i_make
+  end
 end
